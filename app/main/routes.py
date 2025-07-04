b4c6ecd4b15dbe4d062d2c27e20711e53e0f2b99
Almighty42 <xenoman4202@gmail.com>
2025-07-05 00:13:34 +0200

    Added a local JSON file for dummy data, still trying to import it to the existing jinja template. Also added basic .env work as well

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,34 +1,34 @@
+from __future__ import print_function
 from flask import render_template, abort
 from app.main import bp
+import json
+from os import environ
+
+# TODO: REMOVE THIS ONCE THE DATABASE HAS BEEN IMPLEMENTED
+
+import sys
+
+def write_json(path, json_data):
+    with open(path, 'w') as file_out:
+        json.dump(json_data, file_out)
+
+
+def read_json(path):
+    with open(path) as file_in:
+        return json.load(file_in)
 
 @bp.route('/')
 @bp.route('/home')
 def index():
-    
-    articles = [
-        {
-            'id': 23,
-            'title': 'My first article',
-            'created_at': 'August 7, 2024'
-        },
-        {
-            'id': 45,
-            'title': 'My second article',
-            'created_at': 'August 8, 2024'
-        },
-        {
-            'id': 11,
-            'title': 'My third article',
-            'created_at': 'July 27, 2024'
-        },
-        {
-            'id': 101,
-            'title': 'My fourth article',
-            'created_at': 'August 7, 2024'
-        },
-    ]
 
-    return render_template('index.jinja', title="Home", articles=articles)
+    json_articles = read_json(environ.get('PROJECT_PATH') + "app/static/data.json")
+
+    r = json.dumps(json_articles)
+    loaded_r = json.loads(r)
+
+    print(json_articles.article_23, file=sys.stderr)
+
+    return render_template('index.jinja', title="Home", articles=loaded_r)
 
 @bp.route('/article/<int:id>')
 def article(id):
@@ -37,22 +37,27 @@ def article(id):
         {
             'id': 23,
             'title': 'My first article',
-            'created_at': 'August 7, 2024'
+            'created_at': 'August 7, 2024',
+            'content': 'Hello world'
         },
         {
             'id': 45,
             'title': 'My second article',
-            'created_at': 'August 8, 2024'
+            'created_at': 'August 8, 2024',
+            'content': 'Hello world 2'
         },
         {
             'id': 11,
             'title': 'My third article',
-            'created_at': 'July 27, 2024'
+            'created_at': 'July 27, 2024',
+            'content': 'Hello world 3'
+
         },
         {
             'id': 101,
             'title': 'My fourth article',
-            'created_at': 'August 7, 2024'
+            'created_at': 'August 7, 2024',
+            'content': 'Hello world 4'
         },
     ]
 
