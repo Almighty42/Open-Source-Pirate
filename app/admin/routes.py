f8553e3bb12741b9019a1a2376fbfba737b3680a
Almighty42 <xenoman4202@gmail.com>
2025-07-06 11:45:18 +0200

    /new API finished

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,3 +1,4 @@
+from os import environ, path
 from flask import redirect, render_template, flash, request, url_for
 from app.admin import bp
 from app.admin.forms import ArticleForm, LoginForm
@@ -5,6 +6,7 @@ from ..utils import json_handler
 from ..models import User
 from flask_login import login_required, login_user, current_user, logout_user
 from datetime import datetime
+import json
 
 @bp.route("/login", methods=['GET', 'POST'])
 def login():
@@ -38,9 +40,27 @@ def dashboard():
 def new():
     form = ArticleForm()
     if request.method == 'POST' and form.validate():
-        # Code that adds a new article to the data.json DB
+        # Load articles from DB
+        articles = json_handler()
+        # New article definition
+        new_article = {
+            "id": max([article["id"] for article in articles], default=0) + 1,
+            "title": form.title.data,
+            "created_at": datetime.now().strftime('%B %d, %Y'),
+            "content": form.content.data
+        }
+        # Add new article to the local copy of articles DB
+        articles.append(new_article)
+        # Write copy to the DB
+        project_path = environ.get('PROJECT_PATH')
+        if project_path is None:
+            raise ValueError('PROJECT_PATH not set')
+        data_path = path.join(project_path, "app/static/data.json")
+        with open(data_path, 'w') as file:
+            json.dump({f"article_{article['id']}": article for article in articles}, file)
+        # Finished!
         flash('New article added! - article title {}'.format(form.title.data))
         return redirect(url_for("main.index"))
-    form.pub_date.data = datetime.now().strftime('%d-%m-%Y')
+    form.pub_date.data = datetime.now().strftime('%B %d, %Y')
     is_auth = current_user.is_authenticated
     return render_template('new.jinja', title="Add new article", form=form, is_auth=is_auth)
