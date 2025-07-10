2a8c1e3f31d2003f2ed835dde0001d2c05570bf1
Almighty42 <xenoman4202@gmail.com>
2025-07-10 12:57:07 +0200

    Cleaned up the project according to the flaskr tutorial project from flask docs
    Added pyproject.toml with project details, requirements.txt for easier
    installation / deployment of project on other machines and cleaned up
    the main create_app()

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, abort
 from config import Config
 from flask_login import LoginManager
@@ -6,10 +7,25 @@ from flask_login import LoginManager
 login_manager = LoginManager()
 login_manager.login_view = "main.index"
 
-def create_app():
+def create_app(test_config = None):
     # Load configuration
-    app = Flask(__name__)
-    app.config.from_object(Config)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(app.instance_path, '...')
+    )
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_object(Config)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     # Flask Blueprints
     from app.main import bp as main_bp
